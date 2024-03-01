bc2d475d48051e39057590ccf75de8b135b29ff7
ILD <161823650+Guidethegoofy@users.noreply.github.com>
2024-03-02 02:20:14 +0700

    Delete Lab workingw6 directory

--- Lab workingw6/persontest.java
+++
@@ -1,9 +0,0 @@
-public class persontest{
-    public static void main( String args[] ){
-        Person person = new Person( "John", "Smith", 19 );
-        System.out.printf( "Created %s %s, age %d\n",person.getFirstName(), person.getLastName(), person.getAge() );
-        int age = person.getAge() + 1;
-        person.setAge(age);
-        System.out.printf( "Happy Birthday to %s %s\n",person.getFirstName(), person.getLastName());
-    } // end main
-}
