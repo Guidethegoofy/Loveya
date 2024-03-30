5d0fc5ca9a5472e19baed3121306a95fc9a3aa05
ILD <161823650+Guidethegoofy@users.noreply.github.com>
2024-03-30 22:24:53 +0700

    Delete lab 9/main1.java

--- lab 9/main1.java
+++
@@ -1,8 +0,0 @@
-public class main1 {
-    public static void main(String[] args) {
-        Triangle t1 = new Triangle(4.0,2,4,"yellow");
-        System.out.println(t1.toString());
-        System.out.println("Area = "+t1.getArea());
-        t1.print();
-    }
-}
